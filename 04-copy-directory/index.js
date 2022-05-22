b0ba3737062b132ec99d9f677d33fb3540ddf1ab
Anastasiya <anastasiyadrobysh99@gmail.com>
2022-05-22 15:30:06 +0300

    feat: completed the task 1)read file, 2)write file, 4)copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,58 @@
+const fs = require('fs');
 
+const path = require('path');
+
+const src = path.join(__dirname, '../04-copy-directory') + '/files';
+const dest = path.join(__dirname, '../04-copy-directory') + '/files-copy';
+
+function createDir(dest) {
+
+    fs.access(dest, function (err) { 
+        if (err && err.code === 'ENOENT') { 
+            fs.mkdir(dest, {
+                recursive: true
+            }, (error) => {
+                if (error) throw error;
+            }); 
+            copyDir(src, dest)
+        } else { 
+            fs.readdir(dest, (error, fileNames) => {
+                if (error) throw error;
+                fileNames.forEach(filename => {
+
+                    fs.unlink(path.join(dest, filename), (error) => {
+                        if (error) throw error;
+                    })
+                })
+            })
+            copyDir(src, dest)
+        }
+    });
+
+}
+
+function copyDir(src, dest) {
+    fs.readdir(src, {
+        withFileTypes: true
+    }, (error, fileNames) => {
+        if (error) throw error;
+
+        fileNames.forEach(filename => {
+            const srcPath = path.join(src, filename.name);
+            const destPath = path.join(dest, filename.name);
+            if (filename.isDirectory()) {
+                copyDir(srcPath, destPath, (error) => {
+                    if (error) throw error;
+                });
+            } else {
+                fs.copyFile(srcPath, destPath, (error) => {
+                    if (error) throw error;
+                });
+            }
+        })
+    })
+
+    console.log('copy dir successful!')
+}
+
+createDir(dest)
